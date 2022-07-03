98fb10ea5e037fa7c4d4e0540022e871ca55c657
Itsyadavishal <vishalyadav17147714@gmail.com>
2022-07-03 17:31:29 +0530

    Revert "Added realme devicesettings"
    This reverts commit b115ddd2627dec45179a2382b67e2fb748e1c95d.

--- device.mk
+++ device.mk
@@ -188,11 +188,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
     
-# Doze
+# Parts
 PRODUCT_PACKAGES += \
-    devicesettings \
     RealmeProximityHelper \
-    RealmeDoze
+    RealmeParts
 
 # Fingerprint
 PRODUCT_PACKAGES += \
